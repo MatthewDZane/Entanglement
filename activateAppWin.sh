e093151082448a88892c9aa8c8c75221e664e3bc
Matthew Zane <matthewdzane@gmail.com>
2020-09-11 00:37:56 -0700

    Added activate script for Windows users who have ubuntu

---
+++ activateAppWin.sh
@@ -0,0 +1,12 @@
+#1/bin/bash
+# Run using Ubuntu
+# Use to activate application, which will be on http://127.0.0.1:5000
+# assumes you are at top level of repo
+cd client
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+pip install virtualenv
+virtualenv env
+source env/bin/activate
+pip3 install -r requirements.txt
+python app.py
